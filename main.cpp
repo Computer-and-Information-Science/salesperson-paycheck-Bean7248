a478d379603259f56e39d2725e38aa020549b3bc
Tora <TDKING131@GMAIL.COM>
2025-09-16 16:00:03 +0000

    Created code for selecting file name and using file data to create output

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
+//Tora King
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+ //Asking for file name input
+    string fileName;
+cout << "Enter file name: " << endl;
+cin >> fileName;
+
+ifstream inData;
+
+//Took input fileName and use that to find the test file
+inData.open(fileName);
+
+string firstName, lastName;
+double baseSalary, comissionPercent;
+double totalSales;
+double expense;
+double total;
+double totalComission;
+
+//Took data from selected file
+cout << setprecision(2) << fixed;
+inData >> firstName >> lastName >> baseSalary >> comissionPercent >> totalSales >> expense;
+
+totalComission = (comissionPercent * totalSales) / 100;
+total = totalComission + expense + baseSalary;
+
+//Created output for information in file
+cout << "Payroll data for " << firstName << " " << lastName << endl;
+cout << endl;
+cout << "Base Salary:  " << baseSalary << endl;
+cout << "Comission:     " << totalComission << " (" << comissionPercent << "% " << "of " << totalSales << ")" << endl;
+cout << "Expense:       " << expense << endl;
+cout << "     ------------------" << endl;
+cout << "Total:        " << total << endl;
+
+inData.close();
+
+return 0;
 }
